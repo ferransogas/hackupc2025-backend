9809c4a434e5380ed973c2d3193787d47e7a146f
ferran <0ferransogas4@gmail.com>
2025-05-03 15:26:56 +0200

    speech to text

--- main.py
+++ main.py
@@ -3,3 +3,9 @@ from app.handlers import users
 
 app = FastAPI()
 app.include_router(users.router)
+
+import whisper
+
+model = whisper.load_model("base")
+result = model.transcribe("hackupc2025-backend/audio2.mp3", initial_prompt="Note that is possible that one or many of the following names appear: Lila, Claudia, Ferran, María, Andrés, Jhon")
+print(result)
