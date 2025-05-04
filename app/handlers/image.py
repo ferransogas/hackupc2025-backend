1947c9258d1f7a0750ff80d88f804f7c85ec573d
ferran <0ferransogas4@gmail.com>
2025-05-04 04:58:51 +0200

    Enhance image and voice processing with data validation and categorization features

--- app/handlers/image.py
+++ app/handlers/image.py
@@ -8,10 +8,14 @@ import json
 from app.config.config import CONFIG
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.prompts import PromptTemplate
-from langchain.schema import HumanMessage
+from pydantic import BaseModel, ValidationError
+from typing import Dict, List
 
 
 router = APIRouter()
+
+class ShoppingListPrices(BaseModel):
+    food_items: Dict[str, float]
 
 @router.post("/image")
 async def process_image(image_file: UploadFile = File(...)):
@@ -32,7 +36,9 @@ async def process_image(image_file: UploadFile = File(...)):
     # delete the temporary file
     os.unlink(image)
 
-    return extract_items_with_llm(text)
+    extracted = extract_items_with_llm(text)
+
+    return validate_data(extracted, 1)
 
 def image_to_string(path):
     return pytesseract.image_to_string(Image.open(path))
@@ -55,12 +61,12 @@ def extract_items_with_llm(text: str) -> dict:
     }}
 
     Rules:
-    -The keys (products) must match exactly how they appear in the OCR text, as strings.
-    -The values must be doubles: the total sum of all occurrences of each product, rounded to two decimal places.
-    -If a product appears multiple times, add up its prices before converting to a double. Example: '1 Coca Cola 1.50' and then '1 Coca Cola 1.50' = 'Coca Cola': 3.00
-    -Ignore irrelevant lines like totals, taxes, discounts or zero quantities.
-    -Do NOT include anything else: no code fences, no explanations, no extra quotation marks, no markdown, just JSON.
-    -Return only the JSON object.
+    - The keys (products) must match exactly how they appear in the OCR text, as strings.
+    - The values must be doubles: the total sum of all occurrences of each product, rounded to two decimal places.
+    - If a product appears multiple times, add up its prices before converting to a double. Example: '1 Coca Cola 1.50' and then '1 Coca Cola 1.50' = 'Coca Cola': 3.00
+    - Ignore irrelevant lines like totals, taxes, discounts or zero quantities.
+    - Do NOT include anything else: no code fences, no explanations, no extra quotation marks, no markdown, just JSON.
+    - Return only the JSON object.
 
     Receipt text:
     {text}
@@ -82,3 +88,51 @@ def extract_items_with_llm(text: str) -> dict:
         raw = "\n".join(lines)
 
     return json.loads(raw)
+
+def validate_data(data: dict, num_tries: int = 0):
+    # validate the struct of the data
+    try:
+        result = ShoppingListPrices(data)
+        return result
+    
+    except ValidationError as e:
+        if num_tries > 0:
+            # create the data extractor llm and prompt it with the transcribed text
+            llm = ChatGoogleGenerativeAI(model='gemma-3-27b-it', google_api_key=CONFIG['GEMINI_KEY'], temperature=0)
+
+            template = """
+            You are a data correction specialist. I will give you a data and an associated error and you will have to correct it so it is a valid JSON dictionary.
+
+            Rules:
+            - Return a valid JSON dictionary.
+            - Keys: products.
+            - Values: list of people paying for the product.
+            - Do not include explanations.
+            - Do NOT include anything else: no code fences, no explanations, no extra quotation marks, no markdown, just JSON.
+            - Respect corrections, negations and handle human messes.
+
+            Data: {data}
+            Error: {error}
+            """
+
+            prompt = PromptTemplate.from_template(template)
+            chain = prompt | llm
+
+            result = chain.invoke({'data': data, 'error': e})
+            raw = result.content.strip()
+
+            # clean the code in case it's markdown
+            if raw.startswith("```"):
+                lines = raw.splitlines()
+                if lines[0].strip().lower() == "```json":
+                    lines = lines[1:]
+                elif lines[0].strip().startswith("```"):
+                    lines = lines[1:]
+                if lines and lines[-1].strip().startswith("```"):
+                    lines = lines[:-1]
+                raw = "\n".join(lines)
+            
+            return validate_data(json.loads(raw))
+
+        else:
+            raise HTTPException(status_code=500, detail="Please try again.")
--- app/handlers/voice.py
+++ app/handlers/voice.py
@@ -2,18 +2,25 @@ import whisper
 from app.config.config import CONFIG
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.prompts import PromptTemplate
-from fastapi import APIRouter, File, UploadFile, HTTPException
+from fastapi import APIRouter, File, UploadFile, HTTPException, Form
+from app.handlers.users import get_friends
+from pydantic import BaseModel, ValidationError
+from typing import Dict, List
 import os
 import shutil
+import json
 import tempfile
 
 router = APIRouter()
+
+class ShoppingListPeople(BaseModel):
+    food_items: Dict[str, List[str]]
 
 # TODO: relacionar els productes detectats amb els del tiquet
 # TODO: usuaris del initial_prompt ha de ser una crida a la bd
 # TODO: fer json de la resposta
 @router.post("/voice")
-async def process_speech(audio_file: UploadFile = File(...)):
+async def process_speech(audio_file: UploadFile = File(...), products: str = Form(...), user_id: int = Form(...)):
     # Validate file type
     content_type = audio_file.content_type
     if not content_type or not content_type.startswith("audio/"):
@@ -24,16 +31,42 @@ async def process_speech(audio_file: UploadFile = File(...)):
         # copy the uploaded file content to the temporary file
         shutil.copyfileobj(audio_file.file, temp_audio)
         audio = temp_audio.name
-        
-    # convert the audio file to text
-    # load the model
-    model = whisper.load_model('base')
-    # transcribe the audio file knowing the user's friends and translate to english
-    result = model.transcribe(audio, task="translate", initial_prompt='Note that is possible that one or many of the following names appear: Lila, Claudia, Ferran, María, Andrés, Jhon') 
 
+    # parse the products
+    products = json.loads(products)
+        
+    # get user's friends
+    friends = fetch_friends(user_id)
+
+    # convert the audio file to text
+    text = speech_to_text(audio, friends)
+    print(text)
     # delete the temporary file
     os.unlink(audio)
 
+    # extract data from the transcribed text
+    identified = extract_items_with_llm(text)
+
+    # categorize the identified products
+    categorized = categorize_products(identified, products.keys())
+
+    # validate the categorized data
+    return validate_data(categorized, 1)
+    
+
+def fetch_friends(user_id):
+    return [friend['name'] for friend in get_friends(user_id)]
+
+def speech_to_text(audio, friends):
+    # load the model
+    model = whisper.load_model('base')
+    # transcribe the audio file knowing the user's friends and translate to english
+    result = model.transcribe(audio, task="translate",
+                              initial_prompt='Note that is possible that one or many of the following names appear ' + ', '.join(friends))
+    print('Note that is possible that one or many of the following names appear ' + ', '.join(friends))
+    return result['text']
+    
+def extract_items_with_llm(text: str):
     # create the data extractor llm and prompt it with the transcribed text
     llm = ChatGoogleGenerativeAI(model='gemma-3-27b-it', google_api_key=CONFIG['GEMINI_KEY'], temperature=0)
 
@@ -43,13 +76,15 @@ async def process_speech(audio_file: UploadFile = File(...)):
     Your job is to identify what does pay each person and to extract a dictionary where the keys are the product names, and the values are lists of people who will pay for those products.
 
     Return only a valid JSON like this:
-    {{ "pizza": ["Claudia", "Ferran"], "beverages": ["John"], "candies": ["Lila"] }}
+    {{ "pizza": ["Claudia", "Ferran"], "beverages": ["Jhon"], "candies": ["Lila"] }}
+
 
     Rules:
     - Return a valid JSON dictionary.
     - Keys: products.
     - Values: list of people paying for the product.
     - Do not include explanations.
+    - Do NOT include anything else: no code fences, no explanations, no extra quotation marks, no markdown, just JSON.
     - Respect corrections, negations and handle human messes.
 
     Sentence: {sentence}
@@ -58,6 +93,112 @@ async def process_speech(audio_file: UploadFile = File(...)):
     prompt = PromptTemplate.from_template(template)
     chain = prompt | llm
 
-    result = chain.invoke({'sentence': result['text']})
+    result = chain.invoke({'sentence': text})
+    raw = result.content.strip()
+
+    # clean the code in case it's markdown
+    if raw.startswith("```"):
+        lines = raw.splitlines()
+        if lines[0].strip().lower() == "```json":
+            lines = lines[1:]
+        elif lines[0].strip().startswith("```"):
+            lines = lines[1:]
+        if lines and lines[-1].strip().startswith("```"):
+            lines = lines[:-1]
+        raw = "\n".join(lines)
     
-    return result.content
+    return json.loads(raw)
+
+def categorize_products(identified: dict, products: dict):
+    # create the data extractor llm and prompt it with the transcribed text
+    llm = ChatGoogleGenerativeAI(model='gemma-3-27b-it', google_api_key=CONFIG['GEMINI_KEY'], temperature=0)
+
+    template = """
+    You are a product identifier expert and you will have to categorizze them precisely.
+    I will give you a dictionary A of initial products and people who pay for them, and a list B of final products.
+    Your job is to categorize the initial products of the dictionary A precisely according to the products of the dictionary B.
+    As a result, you will have to return a JSON dictionary with the same structure as A, but that contains just the final categorized products along with the people who pay for them.
+
+    Return only a valid JSON like this:
+    {{ "pizza": ["Claudia", "Ferran"], "towel": ["Jhon"], "popcorn": ["Lila"] }}
+
+    Note that a initial product can be just the same as the final one, but an initial product can group some final products, or some initial products can be grouped into a final product.
+    
+    Rules:
+    - Return a valid JSON dictionary.
+    - Keys: products.
+    - Values: list of people paying for the product.
+    - Do not include explanations.
+    - Do NOT include anything else: no code fences, no explanations, no extra quotation marks, no markdown, just JSON.
+    - Respect corrections, negations and handle human messes.
+
+    Dictionary A: {dict_a}
+    List B: {list_b}
+    """
+
+    prompt = PromptTemplate.from_template(template)
+    chain = prompt | llm
+
+    result = chain.invoke({'dict_a': identified, 'list_b': products})
+    raw = result.content.strip()
+
+    # clean the code in case it's markdown
+    if raw.startswith("```"):
+        lines = raw.splitlines()
+        if lines[0].strip().lower() == "```json":
+            lines = lines[1:]
+        elif lines[0].strip().startswith("```"):
+            lines = lines[1:]
+        if lines and lines[-1].strip().startswith("```"):
+            lines = lines[:-1]
+        raw = "\n".join(lines)
+    
+    return json.loads(raw)
+
+def validate_data(data: dict, num_tries: int = 0):
+    # validate the struct of the data
+    try:
+        result = ShoppingListPeople(data)
+        return result
+    
+    except ValidationError as e:
+        if num_tries > 0:
+            # create the data extractor llm and prompt it with the transcribed text
+            llm = ChatGoogleGenerativeAI(model='gemma-3-27b-it', google_api_key=CONFIG['GEMINI_KEY'], temperature=0)
+
+            template = """
+            You are a data correction specialist. I will give you a data and an associated error and you will have to correct it so it is a valid JSON dictionary.
+
+            Rules:
+            - Return a valid JSON dictionary.
+            - Keys: products.
+            - Values: list of people paying for the product.
+            - Do not include explanations.
+            - Do NOT include anything else: no code fences, no explanations, no extra quotation marks, no markdown, just JSON.
+            - Respect corrections, negations and handle human messes.
+
+            Data: {data}
+            Error: {error}
+            """
+
+            prompt = PromptTemplate.from_template(template)
+            chain = prompt | llm
+
+            result = chain.invoke({'data': data, 'error': e})
+            raw = result.content.strip()
+
+            # clean the code in case it's markdown
+            if raw.startswith("```"):
+                lines = raw.splitlines()
+                if lines[0].strip().lower() == "```json":
+                    lines = lines[1:]
+                elif lines[0].strip().startswith("```"):
+                    lines = lines[1:]
+                if lines and lines[-1].strip().startswith("```"):
+                    lines = lines[:-1]
+                raw = "\n".join(lines)
+            
+            return validate_data(json.loads(raw))
+        
+        else:
+            raise HTTPException(status_code=500, detail="Please try again.")
